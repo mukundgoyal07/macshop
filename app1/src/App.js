8ccdea2c2e6e9cfba742188c756573e6520c0cfa
Mukund <mukundgoyal07@gmail.com>
2023-04-06 12:48:13 +0530

    first commit

---
+++ app1/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div>
+       <header className='App-header'>
+        <a href='/'>Macshop</a>
+       </header>
+<main>list products</main>
+    
+    </div>
+  );
+}
+
+export default App;
